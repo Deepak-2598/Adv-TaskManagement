511441226a927543086486da0b8416eb58cc9d89
Deepak <deepaksurya25998@gmail.com>
2024-03-25 17:09:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Signup from './components/Auth/Signup';
+import Login from './components/Auth/Login';
+import Dashboard from './components/shared/dashboard';
+import Profile from './components/shared/profile';
+import Navbar from './components/shared/navbar';
+import { auth } from './firebase';
+
+function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(authUser => {
+      if (authUser) {
+        setUser(authUser);
+        localStorage.setItem('user', JSON.stringify(authUser));
+      } else {
+        setUser(null);
+        localStorage.removeItem('user');
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <BrowserRouter>
+      {user && <Navbar user={user} />}
+      <Routes>
+        <Route path="/" element = {user ? <Navigate to="/dashboard" replace /> : <Navigate to="/login" replace />}/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={user ? <Navigate to="/dashboard" replace /> : <Signup />} />
+        <Route path="/dashboard" element={user ? <Dashboard /> : <Navigate to="/login" replace />} />
+        <Route path="/profile" element={user ? <Profile /> : <Navigate to="/login" replace />} />
+        <Route path="*" element={<Navigate replace to="/404" />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
